8218b6c636e9dbba55ed8e6941d1fbd7ca40bdd6
Julio Scarceli <julioscarcelli31@gmail.com>
2023-11-06 01:03:52 -0300

    Ultimas atualizações do projeto

--- 04 - BeautifulSoup II/news.py
+++ 04 - BeautifulSoup II/news.py
@@ -1,11 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-
-
-lista_noticias = []
-
-
 
 response = requests.get('https://g1.globo.com/') #aqui eu solicitei ao servidor uma resposta do site do G1 com o metodo get
 
@@ -21,37 +15,12 @@ site = BeautifulSoup(content, 'html.parser', ) #aqui criei uma variavel chamada 
 
 
 #HTML DA NOTICIA
-noticias = site.findAll('div', attrs= {'class': 'feed-post-body'} )  #esse metodo chama find, ele serve pra encontrar a tag em que voce esta interessado no html 
+noticia = site.find('div', attrs= {'class': 'feed-post-body'} )  #esse metodo chama find, ele serve pra encontrar a tag em que voce esta interessado no html 
 #nesse metodo find, coloquei o primeiro parametro que é a tag que estou interessado, no caso DIV, e o segundo parametro é qual é o atributo (attrs=)
 #que vai nos ajudar achar aquela tag especifica, pois no html tem varias tags DIV
 
 
-for noticia in noticias:
+#Titulo
+titulo = noticia.find('a', attrs= {'class': 'feed-post-link'})
 
-    #Titulo
-    titulo = noticia.find('a', attrs= {'class': 'feed-post-link'})
-
-    #print(titulo.text) 
-
-    #Subtitulo
-    subtitulo = noticia.find('div', attrs= {'feed-post-body-resumo'})
-
-    if(subtitulo):
-        #print(subtitulo.text)
-        lista_noticias.append([titulo.text, subtitulo.text,titulo['href']])
-    else:
-        lista_noticias.append([titulo.text, '', titulo['href']])
-
-
-news = pd.DataFrame(lista_noticias, columns=['Titulo', 'Subtitulo', 'Link'])
-
-news.to_excel('noticias.xlsx' , index=False)
-
-#print(news)
-
-    
-
-
-
-
-
+print(titulo.text)
